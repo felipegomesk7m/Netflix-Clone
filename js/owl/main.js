3f905db0dc47cfb24836006cad5c2d7a9a25ec61
Felipe Gomes <felipegomesk7m@gmail.com>
2021-10-19 23:42:43 -0300

    Incrementação da animação dos botões do título principal

--- js/owl/main.js
+++ js/owl/main.js
@@ -15,6 +15,7 @@ $('.owl-carousel').owlCarousel({
     }
 })
 
+/* Menu Responsivo */
 const btnMobile = document.getElementById('btn-menumobile');
 
 function toggleMenu() {
@@ -24,3 +25,21 @@ function toggleMenu() {
 
 btnMobile.addEventListener('click', toggleMenu);
 
+/* Botão Assistir Agora */
+const open = document.getElementById('open');
+const modal_container = document.getElementById('modal_container');
+const close = document.getElementById('close');
+const owl_carousel = document.getElementById('owl-carousel');
+
+open.addEventListener('click', () => {
+    modal_container.classList.add('show'),
+    owl_carousel.classList.remove('show')
+})
+
+close.addEventListener('click', () => {
+    modal_container.classList.remove('show')
+})
+
+$(".open").click(function(){
+    $(".owl-carousel").toggleClass("hidden");
+});
